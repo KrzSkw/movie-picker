2f907507938085ac2b02347e323a08bd4fe2749a
Krzysztof Skwiecinski <krzysztof.skwiecinski@elwas.pl>
2020-12-09 21:01:46 +0100

    Working random movie picker. First version ever

--- randomMovie.py
+++ randomMovie.py
@@ -1,5 +1,9 @@
 from get_IMDB_Top import getTopList
+import random
 
 TopMovies = getTopList()
-print(TopMovies[1].find('a').string)
-print(len(TopMovies))
+
+if __name__ == "__main__":
+    movieNumToWatch = random.randint(0, len(TopMovies)-1)
+    printString = "Your randomly picked movie for today is {}. It is {} in IMDB TOP 250. Enjoy!".format(TopMovies[movieNumToWatch].find('a').string, (movieNumToWatch+1))
+    print(printString)
