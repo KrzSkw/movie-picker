b4cf00b759ecd445d472977d0c85316563a514ce
Krzysztof Skwiecinski <krzysztof.skwiecinski@elwas.pl>
2020-12-09 13:49:32 +0100

    Wrapped scrapping script in function

--- get_IMDB_Top.py
+++ get_IMDB_Top.py
@@ -1,16 +1,17 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-top_url = 'https://www.imdb.com/chart/top/?sort=rk,asc&mode=simple&page=1'
-
-page = urlopen(top_url).read()
-soup = BeautifulSoup(page, 'html.parser')
-
-movieNames = soup.find_all('td', attrs={'class': 'titleColumn'})
+def getTopList():
+    top_url = 'https://www.imdb.com/chart/top/?sort=rk,asc&mode=simple&page=1'
+    page = urlopen(top_url).read()
+    soup = BeautifulSoup(page, 'html.parser')
+    movieNames = soup.find_all('td', attrs={'class': 'titleColumn'})
+    return movieNames
 
 
 
 
 if __name__ == "__main__":
-    for movie in movieNames:
+    TopMovies = getTopList()
+    for movie in TopMovies:
         print(movie.find('a').string)
