60a770792b536762def37cadcece1da7fc82c7a8
Krzysztof Skwiecinski <krzysztof.skwiecinski@elwas.pl>
2020-12-14 09:25:00 +0100

    Added method to scrap IMDB rating

--- get_IMDB_Top.py
+++ get_IMDB_Top.py
@@ -1,17 +1,26 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+top_url = 'https://www.imdb.com/chart/top/?sort=rk,asc&mode=simple&page=1'
+page = urlopen(top_url).read()
+soup = BeautifulSoup(page, 'html.parser')
+
 def getTopList():
-    top_url = 'https://www.imdb.com/chart/top/?sort=rk,asc&mode=simple&page=1'
-    page = urlopen(top_url).read()
-    soup = BeautifulSoup(page, 'html.parser')
     movieNames = soup.find_all('td', attrs={'class': 'titleColumn'})
     return movieNames
+
+
+def getTopOver(rating):
+    top_movies = getTopList()
+    top_rating = soup.find_all('td', attrs={'class': 'ratingColumn imdbRating'})
+    for movie in top_rating:
+        print(movie.find('strong').string)
 
 
 
 
 if __name__ == "__main__":
-    TopMovies = getTopList()
-    for movie in TopMovies:
-        print(movie.find('a').string)
+    #TopMovies = getTopList()
+    #for movie in TopMovies:
+    #    print(movie.find('a').string)
+    getTopOver(1)
